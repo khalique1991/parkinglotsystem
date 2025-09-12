df6dd1cbf4a804e2a2d8f154aa87ce8c4fcaa93a
mkhalique1991 <mkhalique1991@gmail.com>
2025-09-12 20:22:28 +0530

    Initial commit - Parking Lot System project

---
+++ src/main/java/com/parkinglotsystem/parkinglotsystem/ParkinglotsystemApplication.java
@@ -0,0 +1,13 @@
+package com.parkinglotsystem.parkinglotsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkinglotsystem/parkinglotsystem/ParkinglotsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkinglotsystem.parkinglotsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
